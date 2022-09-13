dc55fefe74e5c68960486eda278f6aa6bc7c0a08
Debbie Nichols <dnichols346@gmail.com>
2022-09-13 09:16:05 -0700

    Added flash message when registering

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
-  <nav>
+  <div id="flashMessage" v-if="GStore.flashMessage">
+    {{ GStore.flashMessage }}
+  </div>
+  <div id="nav">
     <router-link :to="{ name: 'EventList' }">Events</router-link> |
     <router-link :to="{ name: 'AboutView' }">About</router-link>
-  </nav>
+  </div>
   <router-view />
 </template>
 
+<script>
+export default {
+  inject: ['GStore']
+}
+</script>
+
+
 <style>
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 3s;
+  margin-bottom: 10px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
